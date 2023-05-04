3fc37214773cf3df219f5a6d0e74969d0eea006d
ro0k23 <125355361+ro0k23@users.noreply.github.com>
2023-05-04 09:48:25 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,25 @@
-basic.forever(function () {
-    if (input.magneticForce(Dimension.Strength) < 250) {
-        radio.sendString("ON")
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.InBackground)
-        basic.showIcon(IconNames.Square)
-        basic.pause(100)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber < 0) {
+        basic.showIcon(IconNames.Happy)
+    } else if (receivedNumber < 1) {
+        basic.showIcon(IconNames.Sad)
+        basic.pause(500)
         basic.clearScreen()
-        basic.showIcon(IconNames.SmallDiamond)
-    } else {
-        radio.sendString("OFF")
-        music.stopAllSounds()
-        basic.clearScreen()
+        basic.pause(1000)
     }
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
+    basic.pause(300)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(0)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
+    basic.pause(300)
+    basic.clearScreen()
+})
+radio.setGroup(193)
